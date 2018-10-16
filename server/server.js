cd8a3876dc2ddc4336cc8ee1c555e17d094431c3
wei4118268 <wei814700185@126.com>
2018-10-16 19:20:20 +0800

    优化，增加range请求的处理

--- server/server.js
+++ server/server.js
@@ -1,42 +1,72 @@
-var http=require('http');
-var fs = require('fs');
-var url = require('url');
+let http = require('http');
+let url = require('url');
+let fs = require('fs');
 
-//创建服务器
-http.createServer(function(request,response) {
-  //解析请求，包括文件名
-  var pathname= url.parse(request.url).pathname;
-  //输出请求的文件名
-  console.log("Request for "+ pathname + "  received.");
+let cTypes = {
+    html: 'text/html',
+    css: 'text/css',
+    js: 'application/x-javascript',
+    jpg: 'image/jpg',
+    png: 'image/png',
+    gif: 'image/gif',
+    jpeg: 'image/jpeg',
+    mp4: 'video/mp4'
+}
 
-  var extIndex = pathname.lastIndexOf("\.");
-  var extName = pathname.substring(extIndex + 1);
-  console.log(extName);
-  var ContentType = {'Content-Type': 'text/html'};
-  // js - application/x-javascript
-  if (extName && extName === 'css') {
-    ContentType = {'Content-Type': 'text/css'};
-  } else if (extName && extName === 'js') {
-    ContentType = {'Content-Type': 'application/x-javascript'}
-  }
+//创建server
+http.createServer( (request, response) => {
+    //获取请求的文件名
+    let pathName = url.parse(request.url).pathname;
+    let extName = pathName.substring( pathName.lastIndexOf("\.") + 1 );
 
-  //从文件系统中去请求的文件内容
-  fs.readFile(pathname.substr(1),function(err, data) {
-    if(err) {
-      console.log(err);
-      //HTTP 状态码 404 ： NOT FOUND
-      response.writeHead(404, {'Content-Type': 'text/html'});
+    //输出请求文件名
+    console.log('Request for ' + pathName + ' received.');
+
+    fs.exists( pathName.substring(1), exists => {
+        if( exists ){
+            handleRequest(request, response, pathName, extName);
+        }else{
+            response.writeHead(404, {'Content-Type': 'text/html'});
+            response.end();
+        }
+    } );
+
+    function handleRequest( req, res, pathName, extName ){
+        let range = req.headers['range'];
+        let contentType = cTypes[extName];
+        if( range ){
+            //解析范围请求
+            let totalSize = fs.statSync(pathName.substring(1)).size;
+            //获取range的范围
+            let rangeSize = range.match(/bytes=(\d*)-(\d*)/);
+            rangeSize[2] == '' ? false : rangeSize[2];
+            let end = +(rangeSize[2] || totalSize - 1);
+            let start = +(rangeSize[1]);
+
+            if( !(start > end || start < 0 || end >= totalSize) ){
+                //请求合法
+                res.setHeader('Accept-Ranges', 'bytes');
+                res.setHeader('Content-Range', `bytes ${start}-${end}/${totalSize}`);
+                res.setHeader('Content-Length', start == end ? 0 : end - start + 1);
+                res.setHeader('Content-Type', contentType);
+                res.statusCode = 206;
+
+                //读取文件
+                let rs = fs.createReadStream( pathName.substring(1), { start, end } );
+                rs.pipe(res);
+                rs.on('end', () => {
+                    res.end();
+                });
+            }
+        }else{
+            res.writeHead(200, {'Content-Type': contentType});
+            let rs = fs.createReadStream( pathName.substring(1) );
+            rs.pipe(res);
+            rs.on('end', () => {
+                res.end();
+            });
+        }
     }
-    else {
-      //HTTP 状态码 200 ： OK
-      response.writeHead(200, ContentType);
-
-      //写会回相应内容
-      response.write(data.toString());
-    }
-    //发送响应数据
-    response.end();
-  });
-}).listen(80);
+} ).listen(80);
 
 console.log('Server running at http://127.0.0.1:80/');
